0c86f50aa464b37f305993dc1bcb6c5d4133b887
mktcode <kontakt@markus-kottlaender.de>
2021-02-11 02:40:09 +0100

    updated schema/mapping, added users

--- src/mapping.ts
+++ src/mapping.ts
@@ -15,25 +15,24 @@ export function handleIssueDepositEvent(event: IssueDepositEvent): void {
 }
 
 export function handleOracleAddedEvent(event: OracleAddedEvent): void {
-  const octobay = OctoBay.bind(event.address)
-  const newOracleOnChain = octobay.oracles(event.params.oracle)
+  let octobay = OctoBay.bind(event.address)
 
   // add new oracle
-  const newOracle = new Oracle(event.params.oracle)
+  let newOracle = new Oracle(event.params.oracle.toString())
   newOracle.name = event.params.name
   newOracle.save()
 
-  // load jobs from chain and add them
-  newOracleOnChain.jobs.forEach(job => {
-    const newJob = OracleJob(job.id)
-    newJob.fee = job.fee
-    newJob.oracle = newOracle
-    newJob.save()
-  })
+  // add jobs from chain
+  let registerJobOnChain = octobay.getOracleJob(event.params.oracle, 0)
+  let registerJob = new OracleJob(registerJobOnChain.value0.toString())
+  registerJob.name = 'register'
+  registerJob.fee = registerJobOnChain.value1
+  registerJob.oracle = newOracle.id
+  registerJob.save()
 }
 
 export function handleUserAddedEvent(event: UserAddedEvent): void {
-  const user = new User(event.params.id)
+  let user = new User(event.params.id.toString())
   user.name = event.params.name
   user.ethAddress = event.params.ethAddress
   user.status = event.params.status
