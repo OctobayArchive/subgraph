b89da2f73571b01df525008afb17cb6f34dbbe3c
mktcode <kontakt@markus-kottlaender.de>
2021-04-05 21:08:50 +0200

    fixed oracle mapping

--- src/mappings/oracle-storage.ts
+++ src/mappings/oracle-storage.ts
@@ -1,22 +1,35 @@
-import { OracleStorage, OracleAddedEvent } from '../../generated/OracleStorage/OracleStorage'
+import { store } from '@graphprotocol/graph-ts'
+import { OracleStorage, OracleAddedEvent, OracleRemovedEvent, OracleJobRemovedEvent } from '../../generated/OracleStorage/OracleStorage'
 import { Oracle, OracleJob } from '../../generated/schema'
-// @ts-ignore
-import { getNextNodeId } from './nodeIdCounter'
 
 export function handleOracleAddedEvent(event: OracleAddedEvent): void {
   let oracleStorage = OracleStorage.bind(event.address)
 
   // add new oracle
-  let newOracle = new Oracle(getNextNodeId())
+  let newOracle = new Oracle(event.params.oracle.toHexString())
   newOracle.ethAddress = event.params.oracle
   newOracle.name = event.params.name
   newOracle.save()
 
   // add jobs from chain
   let registerJobOnChain = oracleStorage.getOracleJob(event.params.oracle, 'register')
-  let registerJob = new OracleJob(getNextNodeId())
+  let registerJob = new OracleJob(registerJobOnChain.value0.toString())
   registerJob.name = 'register'
   registerJob.fee = registerJobOnChain.value1
   registerJob.oracle = newOracle.id
   registerJob.save()
+}
+
+export function handleOracleRemovedEvent(event: OracleRemovedEvent): void {
+  let oracle = Oracle.load(event.params.oracle.toHexString())
+  if (oracle) {
+    store.remove('Oracle', oracle.id)
+  }
+}
+
+export function handleOracleJobRemovedEvent(event: OracleJobRemovedEvent): void {
+  let oracleJob = Oracle.load(event.params.id.toString())
+  if (oracleJob) {
+    store.remove('OracleJob', oracleJob.id)
+  }
 }
