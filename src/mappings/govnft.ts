73e6c1591ae04d53c42786855a3eb24cc90ca745
mktcode <kontakt@markus-kottlaender.de>
2021-05-09 22:44:49 +0200

    fixed indexing nft permissions

--- src/mappings/govnft.ts
+++ src/mappings/govnft.ts
@@ -1,11 +1,22 @@
-import { MintNFTForGovTokenEvent, Transfer } from '../../generated/OctobayGovNFT/OctobayGovNFT'
+import { OctobayGovNFT, MintNFTForGovTokenEvent, Transfer } from '../../generated/OctobayGovNFT/OctobayGovNFT'
 import { GovernancePermissionNFT } from '../../generated/schema'
 
 export function handleMintNFTForGovTokenEvent(event: MintNFTForGovTokenEvent): void {
   let nft = new GovernancePermissionNFT(event.params.tokenId.toString())
+  let contract = OctobayGovNFT.bind(event.address)
   nft.ownerAddress = event.params.to
-  nft.permissions = ['MINT', 'TRANSFER', 'SET_ISSUE_GOVTOKEN', 'CREATE_PROPOSAL']
   nft.department = event.params.govTokenAddress.toHexString()
+  nft.permissions = []
+  let permissions = nft.permissions
+  if (contract.hasPermission(event.params.tokenId, 0))
+    permissions.push('MINT')
+  if (contract.hasPermission(event.params.tokenId, 1))
+    permissions.push('TRANSFER')
+  if (contract.hasPermission(event.params.tokenId, 2))
+    permissions.push('SET_ISSUE_GOVTOKEN')
+  if (contract.hasPermission(event.params.tokenId, 3))
+    permissions.push('CREATE_PROPOSAL')
+  nft.permissions = permissions
   nft.save()
 }
 
